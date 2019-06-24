0123b80952ac6b072b8457ae1c30f3ef05ba1fc6
Boyle <jboyle@cdsw.com>
2019-06-24 12:01:00 -0700

    api and data

--- formula1/api.py
+++ formula1/api.py
@@ -5,13 +5,14 @@ from flask import (
     Blueprint, flash, g, request, url_for
 )
 
-from driver.db import get_db
+from formula1.db import get_db
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
-"""def currentrace():
-        db = get_db()
-        error = None
-        db.execute('SELECT * FROM DRIVESEASON').fetchall()
-    
-    return """
+@app.route('/get_race')
+def get_races():
+    raceId = request.args.get('a', 0, type=int)
+    db = get_db()
+    error = None
+    results = db.execute('SELECT TOP(5) * FROM results WHERE raceId = ? ORDER BY rank', (raceId,)).fetchall()
+    return jsonify(results)
